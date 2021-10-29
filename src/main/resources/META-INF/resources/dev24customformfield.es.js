d7a96d36d83132308675d3941675ea44e3caf760
martin-dominguez <martin.dominguez@liferay.com>
2021-10-29 14:46:19 +0200

    2 - Hello World!!

--- src/main/resources/META-INF/resources/dev24customformfield.es.js
+++ src/main/resources/META-INF/resources/dev24customformfield.es.js
@@ -1,49 +1,16 @@
-import 'dynamic-data-mapping-form-field-type/FieldBase/FieldBase.es';
-import './dev24customformfieldRegister.soy.js';
-import templates from './dev24customformfield.soy.js';
-import {Config} from 'metal-state';
-
+import React from 'react';
+import {FieldBase} from 'dynamic-data-mapping-form-field-type/FieldBase/ReactFieldBase.es';
 
 /**
- * Dev24CustomFormField Component
+ * Dev24customformfield React Component
  */
-class Dev24CustomFormField extends Component {
-
-	dispatchEvent(event, name, value) {
-		this.emit(name, {
-			fieldInstance: this,
-			originalEvent: event,
-			value
-		});
-	}
-
-	_handleFieldChanged(event) {
-		const {value} = event.target;
-
-		this.setState(
-			{
-				value
-			},
-			() => this.dispatchEvent(event, 'fieldEdited', value)
-		);
-	}
+const Dev24CustomFormField = (props) => {
+	console.log(props);
+	return (
+		<FieldBase {...props}>
+			<h1>Hello World!</h1>
+		</FieldBase>
+	);
 }
 
-Dev24CustomFormField.STATE = {
-
-	name: Config.string().required(),
-
-	predefinedValue: Config.oneOfType([Config.number(), Config.string()]),
-
-	required: Config.bool().value(false),
-
-	showLabel: Config.bool().value(true),
-
-	spritemap: Config.string(),
-
-	value: Config.string().value('')
-}
-
-// Register component
-Soy.register(Dev24CustomFormField, templates);
-
+export default Dev24CustomFormField;
